85af87b0c8c3ee71d4de0e867e7f7395a86b34ac
Maleehah Zafar <maleehahzafar@Maleehahs-MBP.fios-router.home>
2022-06-19 16:51:46 -0400

    starting stage

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 5000
+
+const app = express()
+// const publicPath = path.join(__dirname, 'build')
+// const p5Path = path.join(__dirname, "p5")
+// app.use(express.static(publicPath))
+// app.use(express.static(p5Path))
+
+app.get("/", (req, res) => {
+    res.send("hello from Heroku!")
+})
+
+// for every other page
+app.get("/*", (req, res) => {
+    res.send("Page is missing!")
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err)
+        console.exit(1)
+    }
+    console.log(`Server Running. Listening on port ${port}`)
+})
